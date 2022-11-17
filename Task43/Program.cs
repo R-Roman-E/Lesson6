8bd37305a0924bb1abc730220a0b3a449dd1ca02
Roman <rom_ich@mail.ru>
2022-11-17 23:17:12 +0300

    1

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -2,20 +2,31 @@
 //пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
- 
- 
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
- 
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
- 
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+double Get(string msg)
+{
+    Console.Write(msg);
+    double userNumber = Convert.ToDouble(Console.ReadLine());
+    return userNumber;
+}
+string FunctionWrite(double a, double b)
+{
+    string result = "y = " + Convert.ToString(a) + "*x + " + Convert.ToString(b);
+    return result;
+}
+string IntersectionCheck(double a, double b, double c, double d)
+{
+    double abscissa = (d - b) / (a - c);
+    double ordinate = abscissa*a + b;
+    string result = Convert.ToString(abscissa) + ";" + Convert.ToString(ordinate);
+
+    return result;
+}
+double numberB1 = Get("Введите коэффициент b1: ");
+double numberK1 = Get("Введите коэффициент k1: ");
+double numberB2 = Get("Введите коэффициент b2: ");
+double numberK2 = Get("Введите коэффициент k2: ");
+Console.WriteLine();
+string function1 = FunctionWrite(numberK1, numberB1);
+string function2 = FunctionWrite(numberK2, numberB2);
+string coordinates = IntersectionCheck(numberK1, numberB1, numberK2, numberB2);
+Console.Write($" Точка пеерсечения двух прямых {function1} и {function2}:\n({coordinates})");
